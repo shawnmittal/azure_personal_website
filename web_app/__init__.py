51a21d84b455b5b5dd3587f309378d97c60d05fe
Shawn Mittal <shawn.mittal@gmail.com>
2020-01-26 21:48:31 -0500

    added bokeh example to projects as blueprint

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from web_app.website.views import mod
-from web_app.bokeh_example.views import mod
+from web_app.bokeh_example.views import dashboard
 
 app.register_blueprint(website.views.mod)
-app.register_blueprint(bokeh_example.views.mod, url_prefix='/projects')
+app.register_blueprint(bokeh_example.views.dashboard, url_prefix='/projects')
--- web_app/bokeh_example/views.py
+++ web_app/bokeh_example/views.py
@@ -1,7 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+from .charting.helper import redraw, components
 
-mod = Blueprint('bokeh_example', __name__)
+dashboard = Blueprint('bokeh_example', __name__,
+                template_folder='templates',
+                static_folder='static',
+                static_url_path='/bokeh_example/static')
 
-@mod.route('/bokeh_example')
+@dashboard.route('/bokeh_example', methods=['GET', 'POST'])
 def bokeh_example():
-    return '<h1>Bokeh Example</h1>'
+    selected_class = request.form.get('dropdown-select')
+
+    if selected_class == 0 or selected_class == None:
+        survived_chart, title_chart, hist_age = redraw(1)
+    else:
+        survived_chart, title_chart, hist_age = redraw(selected_class)
+
+    script_survived_chart, div_survived_chart = components(survived_chart)
+    script_title_chart, div_title_chart = components(title_chart)
+    script_hist_age, div_hist_age = components(hist_age)
+
+    return render_template(
+        'bokeh_example/dashboard.html',
+        div_survived_chart=div_survived_chart,
+        script_survived_chart=script_survived_chart,
+        div_title_chart=div_title_chart,
+        script_title_chart=script_title_chart,
+        div_hist_age=div_hist_age,
+        script_hist_age=script_hist_age,
+        selected_class=selected_class
+    )
