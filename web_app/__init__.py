eb7a1ca6c7fca951dae52348dc43fe1d661ec90e
Shawn Mittal <shawnmittal@pop-os.localdomain>
2020-03-09 20:40:36 -0400

    Changed __init__.py to have correct imports for blueprints

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 from web_app.website.views import mod
 from web_app.bokeh_example.views import dashboard
 
-app.register_blueprint(website.views.mod)
-app.register_blueprint(bokeh_example.views.dashboard, url_prefix='/projects')
+app.register_blueprint(mod)
+app.register_blueprint(dashboard, url_prefix='/projects')
