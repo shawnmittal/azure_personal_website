b6c45425c620eee22ffb919acb01bb892ac4dd8c
Shawn Mittal <shawn.mittal@gmail.com>
2020-08-23 19:30:49 -0400

    started addition of code for nlp_example

---
+++ web_app/nlp_example/parse_and_eval.py
@@ -0,0 +1,16 @@
+import nltk
+from newspaper import Article
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+def get_and_parse(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+    return article
+
+
+def vader(article):
+    sid = SentimentIntensityAnalyzer()
+    return sid.polarity_scores(article.text)
---
+++ web_app/nlp_example/views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, redirect, request, url_for, jsonify, render_template
+
+# NLP packages
+from .parse_and_eval import get_and_parse, vader
+
+nlpExample = Blueprint('nlp_example', __name__,
+                template_folder='templates',
+                static_folder='static',
+                static_url_path='/nlp_example/static')
+
+
+@nlpExample.route('/nlp_example', methods=['POST'])
+def eval_url():
+    url = request.get_json()
+    article = get_and_parse(url)
+    sentiment = vader(article)
+    return jsonify(
+        title=article.title,
+        summary=article.summary,
+        keywords=article.keywords,
+        score=sentiment)
