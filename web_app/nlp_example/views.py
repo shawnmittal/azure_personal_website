d890e80142ac6e85353d267c01358f1b15bd81ff
Shawn Mittal <shawn.mittal@gmail.com>
2020-08-30 13:41:02 -0400

    added template to render nlp frontend

--- web_app/nlp_example/views.py
+++ web_app/nlp_example/views.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Blueprint, redirect, request, url_for, jsonify, render_template
 
 # NLP packages
@@ -19,3 +20,23 @@ def eval_url():
         summary=article.summary,
         keywords=article.keywords,
         score=sentiment)
+
+@nlpExample.route('/nlp_example/frontend', methods=['POST', 'GET'])
+def nlp_frontend():
+    if request.method == 'POST':
+        selected_url = request.form.get('url-select')
+        article = get_and_parse(selected_url)
+        sentiment = vader(article)
+    else:
+        selected_url = 'https://www.cnn.com/2020/08/17/us/coronavirus-college-university/index.html'
+        article = get_and_parse(selected_url)
+        sentiment = vader(article)
+
+    return render_template(
+        'nlp_example/frontend.html',
+        selected_url = article.source_url,
+        article_title = article.title,
+        article_keywords = article.keywords,
+        article_summary = article.summary,
+        article_sentiment = sentiment
+    )
--- web_app/webapp.py
+++ web_app/webapp.py
@@ -1,2 +1,6 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command. 
+
+if __name__ == '__main__':
+    # for local testing
+    app.run(host='127.0.0.1', port=5000, debug=True)
