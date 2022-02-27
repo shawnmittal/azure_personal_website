6b4e5f997ac9db2c2fb71c23e24a6e4f4ff091d6
Shawn Mittal <shawn.mittal@gmail.com>
2022-02-27 10:44:50 -0500

    simplified app structure

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from app import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+   return render_template('index.html')
 
 if __name__ == '__main__':
    app.run()
--- app/main/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from app.main import bp
-
-@bp.route('/', methods=['GET'])
-@bp.route('/index', methods=['GET'])
-def index():
-    return render_template('index.html')
